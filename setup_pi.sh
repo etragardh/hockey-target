4fa9d8790d7d01f055eba80733f983f19e88946a
Emil Trägårdh <emil@tstck.io>
2024-12-30 15:25:51 +0100

    setup

--- setup_pi.sh
+++ setup_pi.sh
@@ -13,13 +13,20 @@ source .venv/bin/activate
 pip3 install adafruit-circuitpython-neopixel 
 # this goes from rpi-lgpio to rpi.gpio
 # # install the original again
-pip3 install rpi-lgpio            # For preassure plates "edge"
-pip3 install smbus                # For LEDs
+pip3 install rpi-lgpio                          # For preassure plates "edge"
+# pip3 uninstall -y RPi.GPIO                    # Fix if fucked up =) run with line below
+# pip3 install --force-reinstall rpi-lgpio      # run this as well for fuckups with "edge"
+pip3 install smbus                              # For LEDs
+pip3 install tabulate                           # For CPrint
 
 # Configure pi for I2C (display)
 # Interfaces -> I2C -> enable -> finnish
 deactivate
-sudo raspi-config nonint do_i2c 1
+sudo raspi-config nonint do_i2c 0
+
+# Neopixels must be run as root
+sudo su
+source .venv/bin/activate
 
 # Setup adhoc wifi
 
